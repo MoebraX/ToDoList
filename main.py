56b9f75a985f2ee7b63b8b461b889abfb2450efa
Mohammad <contact.moebra@gmail.com>
2025-11-30 15:08:23 +0330

    feat: Add the feature to create projects.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import projects
+from projects import *
+
 
 while(True):
-    print("Hi. What can I do for you?")
+    print("\nHi. What can I do for you?")
+    print("""\t Create new project-> 1""")
     command = input()
+    match command:
+        case "1":
+            new_project = create_project()
+            projects_db.append(new_project)
+            print("<Project created successfully!>")
+        case _:
+            pass
--- projects.py
+++ projects.py
@@ -7,3 +7,32 @@ class Project:
         Project._id_counter += 1
         self.name = name
         self.description = description
+
+projects_db: list[Project] = []
+
+def create_project () -> Project:
+    while True:
+        name = str(input("Enter a name: "))
+        if name == "" or name.isspace():
+             print("The name field can't be empty. Try another name.")
+             continue
+        if len(name) > 30:
+            print("Name can't be longer than 30 characters. Try a shorter name.")
+            continue
+        for project in projects_db:
+            if name == project.name:
+                print("This name already exists. Try another name.")
+                continue
+        break
+    while True:
+        description = str(input("Enter a description: "))
+        if len(description) > 150:
+            print("Description can't be longer than 150 characters. Try again.")
+            continue
+        if description == " " or description.isspace():
+            description = "-"
+        break
+    new_project = Project(name, description)
+    return new_project
+    
+       
